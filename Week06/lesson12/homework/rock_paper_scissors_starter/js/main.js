703ce815be37479b19667c6b4a6634328a7c1c6e
Jo Hamlyn <DiscoMountain@Joannas-MacBook-Pro.local>
2016-09-01 20:22:21 +0100

    F&M Commit

--- Week06/lesson12/homework/rock_paper_scissors_starter/js/main.js
+++ Week06/lesson12/homework/rock_paper_scissors_starter/js/main.js
@@ -4,24 +4,25 @@ var computerScore = 0;
 
 // function human clicks rock, scores dont change
 function rock() {
-  $("rock").click;
-  $("status").innerHTML = "The bot played Rock, you draw!";
-  $("humanScore").innerHTML = humanScore;
-  $("computerScore").innerHTML = computerScore;
+  $("#status").html = "The bot played Rock, you draw!";
+  $("#humanScore").html = humanScore;
+  $("#computerScore").html = computerScore;
 }
 
 // function human clicks paper, add one to human score
 function paper() {
-  $("paper").click;
-  $("status").innerHTML = "The bot played Rock, you win!";
-  $("humanScore").innerHTML = humanScore + 1;
-  $("computerScore").innerHTML = computerScore;
+  $("#status").html = "The bot played Rock, you win!";
+  $("#humanScore").html = humanScore + 1;
+  $("#computerScore").html = computerScore;
 }
 
 // function human clicks scissors, add one to computer score
 function scissors() {
-  $("scissors").click;
-  $("status").innerHTML = "The bot played Rock, you loose!";
-  $("humanScore").innerHTML = humanScore;
-  $("computerScore").innerHTML = computerScore + 1;
+  $("#status").html = "The bot played Rock, you loose!";
+  $("#humanScore").html = humanScore;
+  $("#computerScore").html = computerScore + 1;
 }
+
+$("#rock").click(rock);
+$("#paper").click(paper);
+$("#scissors").click(scissors);
